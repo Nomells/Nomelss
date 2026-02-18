e60658c76d85ced5c4baa19598fcf4608d38ca58
Nomells <zjlou_xadrod@mail.ru>
2026-02-18 15:56:16 +0300

    Add files via upload

---
+++ ку/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Hello API"
+)
+
+@app.get("/")
+async def root():
+    return {"message":"Hello from FastAPI"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,host="0.0.0.0", port=8080)
